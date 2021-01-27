524986a8e8330317990c194799920fa24510fe08
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-01-26 21:48:14 -0500

    add leaflet map to the server

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-let ipAddress = "";
+let ip, location, timeZone, isp, lat, lng;
+// var mymap = L.map("mapid").setView([51.505, -0.09], 13);
 
 app.get("/", (req, res) => {
-  res.render("ip", { newAddress: ipAddress });
+  res.render("ip", { newAddress: ip, ispName: isp, time: timeZone });
 });
 
 app.post("/", (req, res) => {
   ipAddress = req.body.newInput;
+
   console.log(ipAddress);
   var api_key = "at_SPsO6PxuGUMOvlwB1YhYNtD6skg4f";
   var api_url = "https://geo.ipify.org/api/v1?";
@@ -26,10 +28,29 @@ app.post("/", (req, res) => {
     response.on("data", (d) => {
       const ipData = JSON.parse(d);
       console.log(ipData);
-    });
-  });
 
-  res.redirect("/");
+      //Location
+      ip = ipData.ip;
+      location = ipData.location;
+      cityName = location.city;
+      region = location.region;
+      postalCode = location.postalCode;
+
+      //Time zone
+      timeZone = location.timezone;
+      console.log(timeZone);
+
+      //ISP
+      isp = ipData.isp;
+      console.log(isp);
+
+      //latitude + longitude
+      lat = location.lat;
+      lng = location.lng;
+      console.log(lat, lng);
+    });
+    res.redirect("/");
+  });
 });
 
 app.listen(3000, () => {
